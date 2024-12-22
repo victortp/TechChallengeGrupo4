ef6388d2c56871ecf380ea840a6cc7924246cd45
Victor Teixeira Pinheiro <teixeirapinheiroster@gmail.com>
2024-12-22 15:17:14 -0300

    refactor: move a classe factory para o arquivo da AppDbContext

--- ContatosGrupo4.Infrastructure/Data/Contexts/AppDbContext.cs
+++ ContatosGrupo4.Infrastructure/Data/Contexts/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using ContatosGrupo4.Application.Configurations;
 using ContatosGrupo4.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
 namespace ContatosGrupo4.Infrastructure.Data.Contexts
@@ -29,6 +31,33 @@ namespace ContatosGrupo4.Infrastructure.Data.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "ContatosGrupo4.Api");
+
+            if (!Directory.Exists(basePath))
+            {
+                throw new DirectoryNotFoundException($"O diretório base '{basePath}' não foi encontrado.");
+            }
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var databaseSettings = new DatabaseSettings
+            {
+                ConnectionString = configuration.GetValue<string>("SqlServer:ConnectionString")
+            };
+
+            var options = Options.Create(databaseSettings);
+
+            return new AppDbContext(options);
         }
     }
 }
