60c85fbcdd5ba10b77e4c8acbff5564fad0458b2
Victor Teixeira Pinheiro <teixeirapinheiroster@gmail.com>
2025-01-10 11:18:30 -0300

    refactor: deixa de validar a existência do usuário e trata qualquer exceção

--- ContatosGrupo4.Application/UseCases/Usuarios/ExcluirUsuarioUseCase.cs
+++ ContatosGrupo4.Application/UseCases/Usuarios/ExcluirUsuarioUseCase.cs
@@ -5,24 +5,19 @@ namespace ContatosGrupo4.Application.UseCases.Usuarios
     public class ExcluirUsuarioUseCase
     {
         private readonly IUsuarioRepository _usuarioRepository;
-        private readonly ObterUsuarioPorIdUseCase _obterUsuarioPorIdUseCase;
 
-        public ExcluirUsuarioUseCase(
-            IUsuarioRepository usuarioRepository,
-            ObterUsuarioPorIdUseCase obterUsuarioPorIdUseCase)
+        public ExcluirUsuarioUseCase(IUsuarioRepository usuarioRepository)
         {
             _usuarioRepository = usuarioRepository;
-            _obterUsuarioPorIdUseCase = obterUsuarioPorIdUseCase;
         }
 
         public async Task ExecuteAsync(int id)
         {
             try
             {
-                var usuarioExistente = await _obterUsuarioPorIdUseCase.ExecuteAsync(id);
                 await _usuarioRepository.ExcluirAsync(id);
             }
-            catch (ArgumentException)
+            catch (Exception)
             {
                 throw;
             }
