c38d45b4ef6cdbb927cb61247e4b4291c4d0ea6b
Victor Teixeira Pinheiro <teixeirapinheiroster@gmail.com>
2025-01-13 20:37:14 -0300

    test: deve lançar exceção quando usuário não existir

---
+++ ContatosGrupo4.Tests/Application/UseCases/AtualizarUsuarioUseCaseTests.cs
@@ -0,0 +1,30 @@
+﻿using ContatosGrupo4.Application.DTOs;
+using ContatosGrupo4.Application.UseCases.Usuarios;
+using ContatosGrupo4.Domain.Entities;
+using ContatosGrupo4.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace ContatosGrupo4.Tests.Application.UseCases
+{
+    public class AtualizarUsuarioUseCaseTests
+    {
+        [Fact]
+        public async Task Deve_Lancar_Excecao_Quando_Usuario_Nao_Existir()
+        {
+            var usuarioRepository = new Mock<IUsuarioRepository>();
+            var obterUsuarioPorIdUSeCase = new ObterUsuarioPorIdUseCase(usuarioRepository.Object);
+            var useCase = new AtualizaUsuarioUseCase(usuarioRepository.Object, obterUsuarioPorIdUSeCase);
+            var dto = new AtualizarUsuarioDto() { Id = 1 };
+
+            usuarioRepository
+                .Setup(r => r.ObterPorIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(default(Usuario));
+
+            var act = async () => await useCase.ExecuteAsync(dto);
+
+            await act.Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Usuário com ID 1 não encontrado.");
+        }
+    }
+}
