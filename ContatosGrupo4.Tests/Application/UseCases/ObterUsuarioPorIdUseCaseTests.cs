bf4ada7bf5f2f1500fb37df108576057c546fbf9
Victor Teixeira Pinheiro <teixeirapinheiroster@gmail.com>
2025-01-13 20:30:21 -0300

    test: deve lançar exceção quando não encontrar o usuário

--- ContatosGrupo4.Tests/Application/UseCases/ObterUsuarioPorIdUseCaseTests.cs
+++ ContatosGrupo4.Tests/Application/UseCases/ObterUsuarioPorIdUseCaseTests.cs
@@ -13,7 +13,7 @@ namespace ContatosGrupo4.Tests.Application.UseCases
         {
             var usuarioRepository = new Mock<IUsuarioRepository>();
             var useCase = new ObterUsuarioPorIdUseCase(usuarioRepository.Object);
-            var usuarioEsperado = new Usuario() { Login = "Loginteste", Senha = "SenhaTeste" };
+            var usuarioEsperado = new Usuario() { Id = 1, Login = "Loginteste", Senha = "SenhaTeste" };
 
             usuarioRepository
                 .Setup(r => r.ObterPorIdAsync(It.IsAny<int>()))
@@ -22,8 +22,25 @@ namespace ContatosGrupo4.Tests.Application.UseCases
             var usuario = await useCase.ExecuteAsync(1);
 
             usuario.Should().NotBeNull();
+            usuario!.Id.Should().Be(1);
             usuario!.Login.Should().Be(usuarioEsperado.Login);
             usuario!.Senha.Should().Be(usuarioEsperado.Senha);
+        }
+
+        [Fact]
+        public async Task Deve_Lancar_Excecao_Quando_Id_Nao_Existir()
+        {
+            var usuarioRepository = new Mock<IUsuarioRepository>();
+            var useCase = new ObterUsuarioPorIdUseCase(usuarioRepository.Object);
+
+            usuarioRepository
+                .Setup(r => r.ObterPorIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(default(Usuario));
+
+            var act = async () => await useCase.ExecuteAsync(1);
+
+            await act.Should().ThrowAsync<ArgumentException>()
+                .WithMessage("Usuário com ID 1 não encontrado.");
         }
     }
 }
